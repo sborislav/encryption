d26c38f2a6e83fe92d49e2f732df5f64a94fc1d3
sborislav <newcait@hotmail.com>
2017-10-31 13:06:14 +0300

    Update server.php

--- coder/server.php
+++ coder/server.php
@@ -48,6 +48,35 @@ class server  extends core
     }
 
 
+    public function Coder( $mes , $key ){
+
+        $mr = $this->hex2bin($mes);
+
+        $mk = $this->hex2bin($key);
+
+        if ( strlen($mr) > strlen($mk)  ){
+            $whole = floor(strlen($mr) / strlen($mk));
+            $distinction = strlen($mr) % strlen($mk);
+
+            $newkey = '';
+            for ($j=0;$j<$whole;$j+=1) $newkey .= $mk;
+            if ( $distinction > 0 )
+                $newkey .= substr($mk, 0, $distinction);
+
+            $mk = $newkey;
+            unset($newkey);
+        }
+
+
+        $bin = '';
+        for ( $i = 0; $i < strlen($mr); $i++ )
+            $bin .= ( $mr[$i] xor $mk[$i] ) ? '1' : '0';
+
+
+        return $this->bin2hex($bin);
+    }
+
+
     /**
      * Получение данных от плагина
      * @return string
@@ -57,10 +86,20 @@ class server  extends core
         switch ($this->_process){
             case 0:
                 $this->_process++;
-                return $this->tryCoder(  $string,  $this->str2hex($this->_key2) );
+                /*
+                Возврат плагну сообщения с нулямя для получения в ответ ключа шифрования (уязвимость)
+                */
+              //  return '0000000000000000000000000000000000000000000000';
+             //   return $this->tryCoder(  '0000000000000000000000000000000000000000000000', $this->str2hex('000') );
+                return $this->tryCoder(  $string, $this->str2hex($this->_key2) );
+
             case 1:
                 $this->_process++;
-                $data = $this->hex2str($this->tryCoder( $string,  $this->str2hex($this->_key2)) );
+               // $data = $this->hex2str($this->tryCoder( $string,  $this->str2hex($this->_key2))) ;
+              //  echo $data;
+               // echo $this->hex2str($string) .'<br>';
+                $data = $this->hex2str($this->tryCoder( $string,  $this->str2hex($this->_key2))) ;
+             //   echo $data;
                 $code = $this->licenceCheck($data);
                 return $this->tryCoder( $this->str2hex( $code),  $this->str2hex($this->_key21) );
             case 2:
